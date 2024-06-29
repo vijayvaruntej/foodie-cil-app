352db1f2ac444bfac02c38b685c7715f1bb017ab
vijayvaruntej <vijayvaruntej30@gmail.com>
2024-06-29 18:05:25 +0530

    feat: add model classes for customer, dish, order, restaurant

--- src/com/triningmug/foodiecilapp/Ui.java
+++
@@ -1,4 +0,0 @@
-package com.triningmug.foodiecilapp;
-
-public class Ui {
-}
---
+++ src/com/triningmug/foodiecilapp/util/CsvReader.java
@@ -0,0 +1,5 @@
+package com.triningmug.foodiecilapp.util;
+
+public class CsvReader {
+    //csv files read and create list of objects
+}
